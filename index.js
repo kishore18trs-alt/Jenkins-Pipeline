a7ff6d2f1ce030b1c035d20cd20fb00c742862be
kishore18trs-alt <kishore18.trs@gmail.com>
2026-04-16 11:46:36 +0530

    commednt added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+// Middleware for express to parse JSON bodies
 app.use(express.json());
 
 // Simple API to test CI/CD
