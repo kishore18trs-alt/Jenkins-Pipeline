62d3100f9bd8ba26759131208a9581d88b694d74
kishore18trs-alt <kishore18.trs@gmail.com>
2026-07-17 11:51:38 +0530

    test coolify auto-deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Simple API to test CI/CD
 app.get('/', (req, res) => {
-res.send('CI/CD Pipeline is working 🚀 — tunnel test!');
+res.send('CI/CD Pipeline & Coolify Deployment  is working 🚀 — tunnel test!');
 
 });
 
